88ffa8151a186bf41df7ca2d0810aab7cd14d352
Matheus Lessa <matheuslessarodrigues@gmail.com>
2017-05-04 12:53:42 -0300

    instancia as coisa

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -9,4 +9,19 @@ public class Grid : MonoBehaviour
 	public int height = 10;
 
 	public Vector2 spacing = new Vector2( 1.0f, 1.0f );
+
+	private void Start()
+	{
+		for( int i = 0; i < width; i++ )
+		{
+			for( int j = 0; j < height; j++ )
+			{
+				bool chooseWhite = ( i * height + j ) % 2 == 0;
+				var position = new Vector3( i * spacing.x, j * spacing.y, 0.0f );
+				Tile tile = Instantiate( chooseWhite ? whiteTilePrefab : blackTilePrefab, position, Quaternion.identity, transform );
+				tile.x = i;
+				tile.y = j;
+			}
+		}
+	}
 }
