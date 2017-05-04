f09ed63279fc5dc1e90be7891afbb5b05c0516ca
Matheus Lessa <matheuslessarodrigues@gmail.com>
2017-05-04 10:04:24 -0300

    tem que instanciar tiles e setupa-los

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -2,8 +2,11 @@
 
 public class Grid : MonoBehaviour
 {
-	public Tile tilePrefab;
+	public Tile blackTilePrefab;
+	public Tile whiteTilePrefab;
 
 	public int width = 10;
 	public int height = 10;
+
+	public Vector2 spacing = new Vector2( 1.0f, 1.0f );
 }
